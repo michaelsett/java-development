1a0fbfb29a804ad4a0638c229ed3234e10130e94
michaelsett <msettle021@gmail.com>
2025-04-10 10:38:59 -0400

    added workshop-1

---
+++ workbook-1/Shopping-list/src/main/java/com/pluralsight/Shoppinglist.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class Shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("milk");
+        System.out.println("eggs");
+        System.out.println("fruit");
+        System.out.println("apple juice");
+        System.out.println("snacks");
+        System.out.println("meat");
+        System.out.println("water bottles");
+        System.out.println("bagels");
+        System.out.println("coffee creamer");
+        System.out.println("candy");
+
+    }
+}
---
+++ workbook-1/firts-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("I wish 2k was good again");
+        System.out.println("like-2k16-to-2k17");
+    }
+}
